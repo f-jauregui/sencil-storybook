78549363c6225491301fe040f0cff65fae29c180
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2020-11-09 14:02:11 -0300

    Updated my component

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -27,6 +27,6 @@ export class MyComponent {
   }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <button class="my-special-button">Hello, FOLKS! I'm {this.getText()}</button>;
   }
 }
