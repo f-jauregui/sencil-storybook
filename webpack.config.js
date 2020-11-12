8f20d1e9b8af789bddeb4a601fc9ee85c3a25671
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2020-11-12 20:04:02 -0300

    Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  },
   optimization: {
     splitChunks: {
       chunks: 'all',
